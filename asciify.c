16265147d59541e352efc7d8a5e13674050ec9a6
duszku <86882994+duszku@users.noreply.github.com>
2022-12-20 19:52:12 +0100

    Moved implementation to a separate module
    Since in this development leap my plan is to introduce command line
    options, parsing and handling them and so on - it will make the source
    code more readable if actual image processing is moved elsewhere.

--- asciify.c
+++ asciify.c
@@ -17,23 +17,10 @@
  */
 
 #define _POSIX_C_SOURCE 200809L
-#include <errno.h>
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
-
-#define VERSION "0.0"
-
-/* report unrecoverable error and exit */
-#define ERROR(X) do {                                       \
-        fprintf(stderr, "[%s:%d] ", __FILE__, __LINE__);    \
-        perror((X));                                        \
-        exit(EXIT_FAILURE);                                 \
-} while (0)
+#include "convert.h"
 
 const char *const def_lvls = " .,;[$&@#";
 
@@ -44,24 +31,7 @@ struct opts {
         unsigned     b_lg : 1;  /* is background light? */
 };
 
-struct image {
-        unsigned char *data;
-        int            w;
-        int            h;
-};
-
-int          read_img(FILE *, struct image *);
-int          largest_i(unsigned char *, int, unsigned char);
-void         print_ascii(struct image *);
-
-/* print usage information and exit */
-void
-usage(void)
-{
-        fprintf(stderr, "USAGE: asciify [options] file\n");
-        fprintf(stderr, "Try \"asciify -h\" for help or consult the manpage\n");
-        exit(EXIT_FAILURE);
-}
+void     usage(void);
 
 /*
  * ASCIIFY is a small and simple image to ascii-art converter that takes a path
@@ -92,61 +62,11 @@ main(int argc, const char **argv)
         return EXIT_SUCCESS;
 }
 
-/* Read image file fp and store result in dest. Return number of pixels */
-int
-read_img(FILE *fp, struct image *dest)
-{
-        memset(dest, 0, sizeof(struct image));
-
-        if (fp == NULL)
-                return 0;
-
-        dest->data = stbi_load_from_file(fp, &(dest->w), &(dest->h), NULL, 1);
-        if (dest == NULL) {
-                fprintf(stderr, "[%s:%d] read_img: input file incorrect\n",
-                    __FILE__, __LINE__);
-                exit(EXIT_FAILURE);
-        }
-
-        return dest->h * dest->w;
-}
-
-/* Find largest index i in sorted array src such that val > src[i] */
-int
-largest_i(unsigned char *src, int size, unsigned char val)
-{
-        int i;
-
-        for (i = 0; i < size; ++i) {
-                if (val < src[i])
-                        break;
-        }
-
-        return i - 1;
-}
-
-/* 1-1 map pixels to characters from lightlevel string and print to stdout */
+/* print usage information and exit */
 void
-print_ascii(struct image *img)
+usage(void)
 {
-        int            i, len;
-        unsigned char *sections;
-
-        len = strlen(def_lvls);
-
-        if ((sections = calloc(len, sizeof(unsigned char))) == NULL)
-                ERROR("calloc");
-
-        for (i = 0; i < len; ++i)
-                sections[i] = i * (UCHAR_MAX / len);
-
-        for (i = 0; i < img->h * img->w; ++i) {
-                if (i % img->w == 0 && i != 0)
-                        putchar('\n');
-
-                putchar(def_lvls[largest_i(sections, len, img->data[i])]);
-        }
-
-        putchar('\n');
-        free(sections);
+        fprintf(stderr, "USAGE: asciify [options] file\n");
+        fprintf(stderr, "Try \"asciify -h\" for help or consult the manpage\n");
+        exit(EXIT_FAILURE);
 }
---
+++ convert.c
@@ -0,0 +1,78 @@
+/*
+ * asciify - simple image to ascii-art converter
+ * Copyright (C) 2022 https://github.com/duszku 
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "convert.h"
+
+/* Read image file fp and store result in dest. Return number of pixels */
+int
+read_img(FILE *fp, struct image *dest)
+{
+        memset(dest, 0, sizeof(struct image));
+
+        if (fp == NULL)
+                return 0;
+
+        dest->data = stbi_load_from_file(fp, &(dest->w), &(dest->h), NULL, 1);
+        if (dest == NULL) {
+                fprintf(stderr, "[%s:%d] read_img: input file incorrect\n",
+                    __FILE__, __LINE__);
+                exit(EXIT_FAILURE);
+        }
+
+        return dest->h * dest->w;
+}
+
+/* Find largest index i in sorted array src such that val > src[i] */
+int
+largest_i(unsigned char *src, int size, unsigned char val)
+{
+        int i;
+
+        for (i = 0; i < size; ++i) {
+                if (val < src[i])
+                        break;
+        }
+
+        return i - 1;
+}
+
+/* 1-1 map pixels to characters from lightlevel string and print to stdout */
+void
+print_ascii(struct image *img)
+{
+        int            i, len;
+        unsigned char *sections;
+
+        len = strlen(def_lvls);
+
+        if ((sections = calloc(len, sizeof(unsigned char))) == NULL)
+                ERROR("calloc");
+
+        for (i = 0; i < len; ++i)
+                sections[i] = i * (UCHAR_MAX / len);
+
+        for (i = 0; i < img->h * img->w; ++i) {
+                if (i % img->w == 0 && i != 0)
+                        putchar('\n');
+
+                putchar(def_lvls[largest_i(sections, len, img->data[i])]);
+        }
+
+        putchar('\n');
+        free(sections);
+}
