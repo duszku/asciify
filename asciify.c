c3c8bab355a400a1a2c886a37a46d1134378f6f3
duszku <86882994+duszku@users.noreply.github.com>
2022-12-20 17:41:02 +0100

    Release 0.0 - basic image to ascii converter
    At this stage program is capable of safely reading an image file and
    producing an ascii output based on light levels of each pixel. It does
    not take any options, it does not have a manual page and bigger images
    will likely not fit on anybody's screen.

--- asciify.c
+++ asciify.c
@@ -1,12 +1,14 @@
 #define _POSIX_C_SOURCE 200809L
 #include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
-#define VERSION "0.0.0"
+#define VERSION "0.0"
 
 /* report unrecoverable error and exit */
 #define ERROR(X) do {                                       \
@@ -15,12 +17,24 @@
         exit(EXIT_FAILURE);                                 \
 } while (0)
 
+const char *const def_lvls = " .,;[$&@#";
+
 struct opts {
         long         widt;      /* width of the output in columns */
         long         heig;      /* height of the output in lines */
         char        *lvls;      /* light levels string (nul = use default) */
         unsigned     b_lg : 1;  /* is background light? */
 };
+
+struct image {
+        unsigned char *data;
+        int            w;
+        int            h;
+};
+
+int          read_img(FILE *, struct image *);
+int          largest_i(unsigned char *, int, unsigned char);
+void         print_ascii(struct image *);
 
 /* print usage information and exit */
 void
@@ -39,5 +53,82 @@ usage(void)
 int
 main(int argc, const char **argv)
 {
+        FILE   *in;
+        struct  image img;
+
+        if (argc != 2)
+                usage();
+
+        if ((in = fopen(argv[1], "rb")) == NULL)
+                ERROR("fopen");
+
+        read_img(in, &img);
+
+        if (fclose(in) == EOF)
+                ERROR("fclose");
+
+        print_ascii(&img);
+
+        stbi_image_free(img.data);
+
         return EXIT_SUCCESS;
+}
+
+/* Read image file fp and store result in dest. Return number of pixels */
+int
+read_img(FILE *fp, struct image *dest)
+{
+        memset(dest, 0, sizeof(struct image));
+
+        if (fp == NULL)
+                return 0;
+
+        dest->data = stbi_load_from_file(fp, &(dest->w), &(dest->h), NULL, 1);
+        if (dest == NULL) {
+                fprintf(stderr, "[%s:%d] read_img: input file incorrect\n",
+                    __FILE__, __LINE__);
+                exit(EXIT_FAILURE);
+        }
+
+        return dest->h * dest->w;
+}
+
+/* Find largest index i in sorted array src such that val > src[i] */
+int
+largest_i(unsigned char *src, int size, unsigned char val)
+{
+        int i;
+
+        for (i = 0; i < size; ++i) {
+                if (val < src[i])
+                        break;
+        }
+
+        return i - 1;
+}
+
+/* 1-1 map pixels to characters from lightlevel string and print to stdout */
+void
+print_ascii(struct image *img)
+{
+        int            i, len;
+        unsigned char *sections;
+
+        len = strlen(def_lvls);
+
+        if ((sections = calloc(len, sizeof(unsigned char))) == NULL)
+                ERROR("calloc");
+
+        for (i = 0; i < len; ++i)
+                sections[i] = i * (UCHAR_MAX / len);
+
+        for (i = 0; i < img->h * img->w; ++i) {
+                if (i % img->w == 0 && i != 0)
+                        putchar('\n');
+
+                putchar(def_lvls[largest_i(sections, len, img->data[i])]);
+        }
+
+        putchar('\n');
+        free(sections);
 }
